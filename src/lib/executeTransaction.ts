35851d0ce18049045ccae2c88f57d5c2cbb72c57
pfed-prog <fedorovn19@gmail.com>
2023-04-23 15:42:35 +0700

    improved formatting

--- src/lib/executeTransaction.ts
+++ src/lib/executeTransaction.ts
@@ -1,16 +1,19 @@
-import { Addresses } from '@/shared/addresses';
-import { TransactionReceipt } from '@ethersproject/abstract-provider';
-import { prepareWriteContract, writeContract } from '@wagmi/core';
+import { Addresses } from "@/shared/addresses";
+import { TransactionReceipt } from "@ethersproject/abstract-provider";
+import { prepareWriteContract, writeContract } from "@wagmi/core";
 
-export const executeTransaction = async (proof: any, publicSignals: Array<string>): Promise<TransactionReceipt> => {
-  const abiPath = require('./abi/SimpleMultiplier.json');
+export const executeTransaction = async (
+  proof: any,
+  publicSignals: Array<string>
+): Promise<TransactionReceipt> => {
+  const abiPath = require("./abi/SimpleMultiplier.json");
 
   // Prepare the transaction data
   const config = await prepareWriteContract({
     address: Addresses.SIMPLE_MULTIPLIER_ADDR,
     abi: abiPath.abi,
-    functionName: 'submitProof',
-    args: [proof, publicSignals]
+    functionName: "submitProof",
+    args: [proof, publicSignals],
   });
 
   // Execute the transaction
@@ -19,4 +22,4 @@ export const executeTransaction = async (proof: any, publicSignals: Array<string
   // Wait for the transaction block to be mined
   const txResult = await writeResult.wait();
   return txResult;
-}
+};
